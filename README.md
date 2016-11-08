fde311c552effd7ac93a6d5942e09a379b4529aa
Russell <2russellsmith@gmail.com>
2016-11-07 22:38:37 -0700

    Logging part

---
+++ README.md
@@ -0,0 +1,37 @@
+krl-parse
+=========
+
+Simple command line tool to validate KRL.
+
+Sample usage:
+
+```krl-parse.pl -f a16x144.krl```
+
+validates the KRL in the named file. 
+
+```krl-parse.pl -?```
+
+returns help message. 
+
+You should consider using the included Github pre-commit hook to automate parsing rulesets before they're checked into Github. 
+
+Proudly crafted in Perl. 
+
+Depends on 
+
+	Getopt::Std;
+	LWP::UserAgent;
+	HTTP::Request;
+	URI::Escape;
+	JSON
+
+If you do not have some of these packages and you're not a Perl person, you first need to [install cpanminus](http://cpanmin.us) by running the following command:
+
+	curl -L http://cpanmin.us | perl - App::cpanminus
+
+If you don't have curl but wget, replace `curl -L` with `wget -O -`.
+
+Then you can use cpanm to install the missing dependencies as follows:
+
+	cpanm -S JSON
+
---
+++ krl-parse.pl
@@ -0,0 +1,93 @@
+#!/usr/bin/perl -w
+use strict;
+
+use Getopt::Std;
+use LWP::UserAgent;
+use HTTP::Request;
+use URI::Escape;
+use JSON -support_by_pp;
+
+#use Data::Dumper;
+
+# global options
+use vars qw/ %opt /;
+my $opt_string = '?hvf:';
+getopts( "$opt_string", \%opt ) or usage();
+
+usage() if $opt{'h'} || $opt{'?'};
+
+my $krl_file = $opt{'f'};
+
+# get the KRL file to validate
+my $ruleset;
+if (-e $krl_file) {
+  local $/;
+  open(KRL, $krl_file);
+  $ruleset = <KRL>;
+  close(KRL)
+    or warn "Unable to close the file handle: $!";
+} else {
+  die "File $$krl_file not found";
+}
+
+# print $ruleset;
+
+my $krl_validate_url = "http://cs.kobj.net/manage/validate/";
+my $krl_flush_url = "http://cs.kobj.net/ruleset/flush/";
+
+my $parameters = ['flavor' => 'json',
+#		  'rule' => uri_escape($ruleset)
+		  'rule' => $ruleset
+		  ];
+
+my $ua = LWP::UserAgent->new(); 
+my $response = $ua->post($krl_validate_url, $parameters);
+
+#print $response->content;
+
+my $json = JSON->new->allow_nonref;
+my $content = eval {
+    $json->allow_singlequote->decode($response->content)
+};
+die "Parser failed miserably: ", $response->content if $@;
+
+
+
+if ($content->{'status'} eq 'error') {
+  print $content->{'result'}, "\n";
+} elsif (($content->{'status'} eq 'success')) {
+  if ($opt{'v'}) {
+    $json = $json->pretty(1);
+    $json = $json->indent_length(4);
+    print $json->encode($content->{'result'}), "\n";
+  } else {
+    print "OK\n";
+  }
+} else {
+  warn "Invalid content returned";
+}
+
+
+1;
+
+
+
+#
+# Message about this program and how to use it
+#
+sub usage {
+    print STDERR << "EOF";
+
+Validates a KRL ruleset
+
+usage: $0 [-h?] -f filename
+
+ -h|?       : this (help) message
+ -f file    : input file
+ -v         : be verbose (return parse tree)
+
+example: $0 -f a16x565.krl
+
+EOF
+    exit;
+}
